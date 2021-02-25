fef4dd79373e4ad5bec8e90c0d89ea0f548fbe44
veeru153 <32760644+veeru153@users.noreply.github.com>
2021-02-26 01:05:50 +0530

    Visual Bug Fixes + Code Cleanup

--- src/containers/Settings/forms/AddWidgetForm.js
+++ src/containers/Settings/forms/AddWidgetForm.js
@@ -1,20 +1,11 @@
 import React from 'react';
-// import classes from './AddWidgetForm.module.css';
 import classes from '../Settings.module.css';
-import FormTemplate from '../FormTemplate.js'
 import Option from '../Option';
 import * as widgetForms from '../../../widgetForms';
 
 const AddWidgetForm = () => {
     const formList = Object.values(widgetForms);
     return (
-        // <FormTemplate
-        //     title="Add Widget"
-        //     subtitle="Choose a Widget:"
-        //     formClasses={classes.AddWidgetForm}
-        // >
-        //     {formList.map(form => <Option key={form.key} opt={form} style={{ height: 283.75 }} />)}
-        // </FormTemplate>
         <div className={classes.Settings}>
         <header>
             <h1>Add Widget</h1>
