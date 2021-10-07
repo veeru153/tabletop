45899472651c03b117d1395cf345a57d1a2d674f
veeru153 <32760644+veeru153@users.noreply.github.com>
2021-10-07 19:40:36 +0530

    publish: Update package script - git is so complicated bruh
    generated from commit 4ac318a43ca9489db9b638e645abd112fd8ea8d8

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.938ab3f6bb5ee5c898a928579f6b0692.js"
+  "./precache-manifest.5676bf162a34e35fd7b7947f302fc924.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
