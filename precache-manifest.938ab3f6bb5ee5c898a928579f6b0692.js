446a719fed3e6bcf952a0fde4b765d7d333f15a3
veeru153 <32760644+veeru153@users.noreply.github.com>
2021-10-07 19:27:21 +0530

    publish: Merge pull request #21 from veeru153/web-dev
    generated from commit 775cb45dd878e7a8fd25aee85d2baae3220a8101

---
+++ precache-manifest.938ab3f6bb5ee5c898a928579f6b0692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df68e6936109942c5a0b65039c79a5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89de84a5f14a2806ed48",
+    "url": "/static/css/main.7ac540e3.chunk.css"
+  },
+  {
+    "revision": "d2f3adeffe551ab1dc17",
+    "url": "/static/js/2.34e8989d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.34e8989d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89de84a5f14a2806ed48",
+    "url": "/static/js/main.7f2a9390.chunk.js"
+  },
+  {
+    "revision": "24b612c201485ea9fd33",
+    "url": "/static/js/runtime-main.e7c7ba29.js"
+  }
+]);
