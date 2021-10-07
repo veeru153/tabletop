0eece7cd0226bd51897a1824034d7a7b3ad43f4d
veeru153 <32760644+veeru153@users.noreply.github.com>
2021-10-07 18:57:00 +0530

    Bug Fix 2: Electric Boogaloo

--- src/containers/Background/Video.js
+++ src/containers/Background/Video.js
@@ -1,20 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import classes from './Background.module.scss';
+import React from 'react';
 
-const Video = () => {
-    const [src, setSrc] = useState("");
-
-    useEffect(() => {
-        async function onMount() {
-            const i = Math.floor(Math.random() * urls[0].length);
-            setSrc(urls[0][i]);
-        }
-        onMount();
-    }, [])
-
-    return (
-        <video src={src} className={classes.Video} autoPlay loop muted controls={false} />
-    )
-}
+const Video = () => (<div></div>);
 
 export default Video;
